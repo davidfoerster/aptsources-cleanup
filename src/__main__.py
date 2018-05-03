5f569f5382566dc6dccd1ff3882f678b5960281a
David Foerster <david.foerster@informatik.hu-berlin.de>
2018-05-03 12:07:43 +0200

    Improve site-packages path removal

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3 -OEs
 import sys
-sys.path[:] = [p for p in sys.path if 'site-packages' not in p]
+sys.path[:] = [p for p in sys.path if 'site-packages' not in p.split('/')]
 
 import runpy
 #_magic = '@@aptsources-cleanup@@'
