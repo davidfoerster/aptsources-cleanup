940c910e10f0f3e3a707bd2f9e9a07b9007fe09e
David Foerster <david.foerster@informatik.hu-berlin.de>
2018-05-08 04:34:06 +0200

    3to2: Runpy needs a byte string modules name

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3 -OEs
-from __future__ import print_function, division, absolute_import, unicode_literals
+from __future__ import print_function, division, absolute_import
 
 import sys
 sys.path[:] = [p for p in sys.path if 'site-packages' not in p.split('/')]
