e4c84ee68b43b8ec0f23f2cc1f5510383a759c79
David Foerster <david.foerster@informatik.hu-berlin.de>
2018-05-08 02:37:42 +0200

    3to2: Convert input() to return Unicode strings

--- src/aptsources_cleanup/util/_3to2.py
+++ src/aptsources_cleanup/util/_3to2.py
@@ -1,11 +1,24 @@
-from __future__ import \
-	print_function, division, absolute_import, unicode_literals
+from __future__ import print_function, division, absolute_import
+
 
 try:
 	from __builtin__ import \
-		unicode as str, str as bytes, raw_input as input, xrange as range
+		unicode as str, str as bytes, basestring, raw_input, \
+		xrange as range
+
 except ImportError:
-	pass
+	from builtins import input
+	basestring = (str, bytes)
+
+else:
+	__all__ = tuple(map(bytes, __all__))
+
+	import locale
+
+	def input(prompt=None):
+		answer = raw_input() if prompt is None else raw_input(prompt)
+		return str(answer, locale.getpreferredencoding())
+
 
 try:
 	from future_builtins import *
