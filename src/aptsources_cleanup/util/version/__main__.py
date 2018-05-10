5f40997d804b640b758087cd26e676cb1cf74db5
David Foerster <david.foerster@informatik.hu-berlin.de>
2018-05-11 01:51:05 +0200

    version: Set output stream locale for 3to2

--- src/aptsources_cleanup/util/version/__main__.py
+++ src/aptsources_cleanup/util/version/__main__.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8
 from __future__ import print_function, division, absolute_import, unicode_literals
 from .._3to2 import *
-import sys
+import sys, locale
 from . import version_info
 
+
+locale.setlocale(locale.LC_ALL, '')
 
 if len(sys.argv) > 1:
 	version = sys.argv[1]
 else:
 	version = input()
 
-version_info.from_repo(version)._print_data_module()
+output = sys.stdout
+if not output.encoding:
+	import codecs
+	encoding = locale.getpreferredencoding()
+	output = codecs.getwriter(encoding)(output)
+	output.encoding = encoding
+
+with output:
+	version_info.from_repo(version)._print_data_module(output)
