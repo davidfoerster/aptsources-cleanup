8b9c3104df18f337805a659de0e4ec8c2ce57bba
David Foerster <david.foerster@informatik.hu-berlin.de>
2018-05-10 21:25:17 +0200

    Move ChainMap implementation to separate module

---
+++ src/aptsources_cleanup/util/impl/chainmap.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8
+from __future__ import absolute_import, unicode_literals
+from .._3to2 import *
+from ..operator import methodcaller
+import operator
+
+__all__ = ('ChainMap',)
+
+
+class ChainMap(object):
+
+	def __init__(self, *underlying):
+		self._data = underlying or ({},)
+
+
+	def _filter_maps_with_key(self, key):
+		return filter(methodcaller(operator.contains, key), self._data)
+
+
+	def __getitem__(self, key):
+		try:
+			return next(iter(self._filter_maps_with_key(key)))[key]
+		except StopIteration:
+			raise KeyError
+
+
+	def get(self, key, default_value=None):
+		try:
+			return next(iter(self._filter_maps_with_key(key)))[key]
+		except StopIteration:
+			return default_value
