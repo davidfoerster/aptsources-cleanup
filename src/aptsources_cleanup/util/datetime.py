cd43c50154cd20b901bbc4b392789c1594133104
David Foerster <david.foerster@informatik.hu-berlin.de>
2018-06-02 03:21:53 +0200

    datetime: Add missing __all__ entry

--- src/aptsources_cleanup/util/datetime.py
+++ src/aptsources_cleanup/util/datetime.py
@@ -3,19 +3,19 @@
 functonality if any.
 """
 from __future__ import absolute_import, unicode_literals
-from datetime import *
 
 
 import datetime as _datetime
 try:
-	__all__ = _datetime.__all__
+	__all__ = list(_datetime.__all__)
 except AttributeError:
 	from operator import methodcaller
 	from .itertools import filterfalse
-	__all__ = tuple(filterfalse(methodcaller('startswith', '_'), dir(_datetime)))
+	__all__ = list(filterfalse(methodcaller('startswith', '_'), dir(_datetime)))
 
 
-try:
-	timezone
-except NameError:
+from datetime import *
+
+if 'timezone' not in __all__:
+	__all__.append('timezone')
 	from .impl.timezone import timezone
