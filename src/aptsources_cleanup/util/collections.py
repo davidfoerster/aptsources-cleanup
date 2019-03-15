6fd90fec61fdf3b03c11a8c3bec8f4539421c0c4
David Foerster <david.foerster@informatik.hu-berlin.de>
2019-03-15 19:49:57 +0100

    ExtSet: documentation strings

--- src/aptsources_cleanup/util/collections.py
+++ src/aptsources_cleanup/util/collections.py
@@ -11,8 +11,11 @@ __all__ += _collections.__all__
 
 
 class ExtSet(set):
+	"""Some extensions to the built-in set class"""
 
 	def add(self, x):
+		"""Same as set.add(). Returns True if the set was changed."""
+
 		l = len(self)
 		super().add(x)
 		return l != len(self)
