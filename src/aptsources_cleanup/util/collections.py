952df4d9148b2e85d25a48596b54cc05005bd2f7
David Foerster <david.foerster@informatik.hu-berlin.de>
2019-10-15 12:36:51 +0200

    util.relations: Major refactoring and various small fixes

--- src/aptsources_cleanup/util/collections.py
+++ src/aptsources_cleanup/util/collections.py
@@ -24,3 +24,21 @@ class ExtSet(set):
 		l = len(self)
 		super().add(x)
 		return l != len(self)
+
+
+	def discard(self, x):
+		"""Same as set.discard(). Returns True if the set was changed."""
+
+		l = len(self)
+		super().discard(x)
+		return l != len(self)
+
+
+	def discard_first_of(self, iterable, default=None):
+		"""Removes and returns the first element that is a member of this set.
+
+		Elements are drawn from an iterable. If none of them is a member of this
+		set, the default value is returned instead.
+		"""
+
+		return next(filter(self.discard, iterable), default)
--- src/aptsources_cleanup/util/relations.py
+++ src/aptsources_cleanup/util/relations.py
@@ -3,100 +3,118 @@ __all__ = ('EquivalenceRelation',)
 
 import itertools
 from operator import methodcaller
-from functools import partial as fpartial
+from .functools import comp, partial as fpartial
+from .collections import ExtSet
+
+
+class FrozensetAltRepr(frozenset):
+
+	__slots__ = ()
+
+	def __str__(self):
+		return ', '.join(tuple(map(repr, self))).join(('{', '}'))
 
 
 class EquivalenceRelation(frozenset):
 
+	__slots__ = ()
+
+
 	def __new__(cls, *classes):
 		if len(classes) == 1:
 			classes, = classes
-		er = super().__new__(cls, map(frozenset, classes))
+
+		self = super().__new__(cls, map(FrozensetAltRepr, classes))
+		if not self:
+			try:
+				return cls.EMPTY
+			except AttributeError:
+				pass
 
 		if __debug__:
 			overlapping = [
-				'{!r} ⋂ {!r} = {!r}'.format(set(a), set(b), set(a & b))
-				for a, b in itertools.combinations(er, 2) if not a.isdisjoint(b)
+				'{} & {} = {}'.format(a, b, FrozensetAltRepr.__str__(a & b))
+				for a, b in itertools.combinations(self, 2) if not a.isdisjoint(b)
 			]
-			if overlapping:
-				raise AssertionError(
-					'Overlapping equivalence classes: ' + ', '.join(overlapping))
+			assert not overlapping, \
+				'Overlapping equivalence classes: ' + ', '.join(overlapping)
 
-		return er
+		return self
 
 
-	def get_class(self, element, default=None):
-		for clazz in self:
-			if element in clazz:
-				return clazz
-		return default
+	def get_class(self, element):
+		return next(filter(methodcaller('__contains__', element), self), None)
 
 
 	@classmethod
 	def parse(cls, s, item_delimiter=',', class_delimiter=';'):
+		if item_delimiter == class_delimiter:
+			raise ValueError(
+				'Item and class delimiters must not be equal ({!r})'
+					.format(item_delimiter))
 		return cls(map(
 			methodcaller('split', item_delimiter), s.split(class_delimiter)))
+
+
+	def __str__(self):
+		return format(self)
 
 
 	def __format__(self, fmt):
 		classes = self
 
-		if fmt:
+		if fmt != '':
 			fmt_orig = fmt
-			fmt = fmt[1:].split(fmt[0])
+			fmt = fmt[1:].split(fmt[0], 6)
+			if len(fmt) < 2:
+				raise ValueError('Illegal format: ' + repr(fmt_orig))
+
 			if len(fmt) % 2:
 				classes, item_transform = (
 					self._format_parse_options(fmt.pop(), classes))
 			else:
 				item_transform = None
-			if len(fmt) == 6:
-				prefix_suffix = fmt[4:]
-				del fmt[4:]
-			else:
-				prefix_suffix = ('', '')
-			if len(fmt) == 4:
-				class_prefix_suffix = fmt[2:]
-				del fmt[2:]
-			else:
-				class_prefix_suffix = ('', '')
-			if len(fmt) == 2:
-				class_delimiter = fmt.pop()
-				item_delimiter = fmt.pop()
-			else:
-				raise ValueError('Illegal format: ' + repr(fmt_orig))
+
+			item_delimiter = fmt[0]
+			class_delimiter = fmt[1]
+			class_prefix_suffix = fmt[2:4]
+			prefix_suffix = fmt[4:6]
 
 		else:
+			item_transform = repr
 			item_delimiter = ', '
 			class_delimiter = '; '
-			item_transform = repr
 			prefix_suffix = class_prefix_suffix = ('{', '}')
 
-		if item_transform is not None:
-			classes = map(fpartial(map, item_transform), classes)
-
-		s = class_delimiter.join(tuple(
-			map(methodcaller('join', class_prefix_suffix),
-				map(item_delimiter.join, classes))))
-		return s.join(prefix_suffix)
+		if item_transform:
+			classes = map(comp(fpartial(map, item_transform), tuple), classes)
+		classes = map(item_delimiter.join, classes)
+		if any(class_prefix_suffix):
+			classes = map(methodcaller('join', class_prefix_suffix), classes)
+		classes = class_delimiter.join(tuple(classes))
+		if any(prefix_suffix):
+			classes = classes.join(prefix_suffix)
+		return classes
 
 
 	@classmethod
 	def _format_parse_options(cls, s, classes):
-		opts = set(s)
+		opts = ExtSet(s)
 
-		try:
-			sort_mode = next(filter(opts.__contains__, ('a', 'd')), None)
-			if sort_mode is not None:
-				opts.remove(sort_mode)
-				classes = sorted(classes, reverse=sort_mode == 'd')
+		sort_mode = opts.discard_first_of(('a', 'd'))
+		if sort_mode is not None:
+			sort_mode = fpartial(sorted, reverse=sort_mode == 'd')
+			classes = sort_mode(map(sort_mode, classes))
 
-			if opts:
-				item_transform, = opts
-			else:
-				item_transform = ''
-			item_transform = cls._item_transformers[item_transform]
+		if not opts:
+			item_transform = ''
+		elif len(opts) == 1:
+			item_transform, = opts
+		else:
+			item_transform = None
 
-		except (TypeError, KeyError):
+		item_transform = cls._item_transformers.get(item_transform, '')
+		if item_transform == '':
 			raise ValueError('Invalid format option string: ' + repr(s))
 
 		return (classes, item_transform)
@@ -119,5 +137,5 @@ class IndexedEquivalenceRelation(EquivalenceRelation):
 			zip(clazz, itertools.repeat(clazz)) for clazz in self))
 
 
-	def get_class(self, element, default=None):
-		return self._index.get(element, default)
+	def get_class(self, element):
+		return self._index.get(element)
