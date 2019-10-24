6787450fa19af4e298c64c5fba6371372858edac
David Foerster <david.foerster@informatik.hu-berlin.de>
2019-10-24 02:20:19 +0200

    main: Handle alternative directory separators

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3 -OEs
 # -*- coding: utf-8
 
-import sys
-sys.path[:] = [p for p in sys.path if 'site-packages' not in p.split('/')]
+import os, sys
+sys_path = sys.path
+if os.altsep:
+	sys_path = ( p.replace(os.altsep, os.sep) for p in sys_path )
+sys.path[:] = [ p for p in sys_path if 'site-packages' not in p.split(os.sep) ]
 
 import runpy
 runpy.run_module('aptsources_cleanup', run_name='__main__')
