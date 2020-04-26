43d16f374c93e1b20febc84ba5bedf04c5f49a43
joncrall <erotemic@gmail.com>
2020-04-26 17:22:05 -0400

    Re-add main script for the pyz distribution

---
+++ src/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3 -OEs
+# -*- coding: utf-8
+# Main script for the pyz distribution
+
+import os, sys
+sys_path = sys.path
+if os.altsep:
+	sys_path = ( p.replace(os.altsep, os.sep) for p in sys_path )
+sys.path[:] = [ p for p in sys_path if 'site-packages' not in p.split(os.sep) ]
+del sys_path
+
+import runpy
+runpy.run_module('aptsources_cleanup', run_name='__main__')
