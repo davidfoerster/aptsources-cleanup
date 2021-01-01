58192c915b26a2f097c9d1ee5855d91357a0e9e5
David Foerster <david.foerster@informatik.hu-berlin.de>
2021-01-01 18:32:54 +0100

    main: Simplify site-packages filtering

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3 -OEs
 # -*- coding: utf-8
 
-import os, sys
-sys_path = sys.path
-if os.altsep:
-	sys_path = ( p.replace(os.altsep, os.sep) for p in sys_path )
-sys.path[:] = [ p for p in sys_path if 'site-packages' not in p.split(os.sep) ]
-del sys_path
+import sys
+from pathlib import PurePath
+sys.path = [ p for p in sys.path if "site-packages" not in PurePath(p).parts ]
 
 import runpy
 runpy.run_module('aptsources_cleanup', run_name='__main__')
